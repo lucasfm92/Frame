37f5761f5d245d1a6630cbe9d4cafe9685595b54
Lucas Furtado de Macedo <lucasmacedo@STUREC.INT>
2019-09-27 16:29:14 -0300

    v03

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,28 +6,33 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona (event) {
 
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
         
-        let data = new Date(...
-
-            this._inputData.value
-            .split('-')
-            .map((item, indice)=> item - indice % 2)
-
-        );
-
-        let negociacao = new Negociacao (
-            data,
+    }   
+    
+    _criaNegociacao() {
+        
+        return new Negociacao (
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
-
-        console.log(negociacao);
+            this._inputValor.value);
 
     }
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
+    }
+
 }
